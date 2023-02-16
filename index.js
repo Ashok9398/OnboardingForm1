d686b6ffd7e1f3cde85a0036694659f6d52a55ef
Ashok9398 <n171112@rguktn.ac.in>
2023-02-16 14:31:41 +0530

    initial

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const app = express();
-const connectDB = require('./db/connect');
-require('dotenv').config();
-const route = require("./routes/router");
+var express =require('express');
+var app=express();
+var body_parser = require('body-parser');
+const router = require('./routes/router');
+const connectDB = require('./db/connect'); 
 
-app.use(express.json());
-
-app.use('/api/v1',books);
 app.get('/',(req,res)=>{
-	res.json("hello");
-})
+  
+  res.send("Hello");
+  
+});
+var json = body_parser.json();
+app.use('/',json,router);
 
 const start = async()=>{
 	try{
 		await connectDB(process.env.MONGO_URI);
-		app.listen(3000);
+		app.listen(8000);
 	}
 	catch(error){
 		console.log(error);
